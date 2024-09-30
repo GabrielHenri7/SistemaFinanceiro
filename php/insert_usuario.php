e336c9e6c3ab2d6c6a2082213beff49daa00f17b
GabrielHenri7 <duartehenri0078@gmail.com>
2024-09-30 20:48:52 -0300

    Finaliza Programa

---
+++ php/insert_usuario.php
@@ -0,0 +1,27 @@
+<?php
+    include 'conexao.php';
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $cargo = $_POST['cargo'];
+    $senha = $_POST['senha'];
+
+    $sql = "INSERT INTO tb_user VALUES (null, '$nome', '$email', '$cargo', '$senha')";
+
+    
+
+    if ($conexao ->query($sql))
+    {
+        echo "<script>('Inserido com Sucesso!'; history.back())<script>";
+    }
+
+    else
+    {
+        echo "Falha na conexão com o banco de dados"; 
+    }
+
+
+
+
+
+?>
