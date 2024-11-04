e41d70fa4224f34b800da5fc50781f2f46101a10
GabrielHenri7 <duartehenri0078@gmail.com>
2024-11-04 18:47:07 -0300

    Finaliza programa

---
+++ php/insert_categoria.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'conexao.php';
+
+$nome = $_POST['categoria'];
+$tipo = $_POST['tipo'];
+$descricao = $_POST['descricao'];
+
+$sql = "INSERT INTO tb_categoria VALUES (null, '$nome', $tipo, '$descricao')";
+
+if ($conexao->query($sql)){
+    echo "<script>alert('Enviado com sucesso'); window.location.href = '../categoria.html';</script>";
+} else {
+    echo "Falha na conexão com o banco de dados";
+}
+
+?>
